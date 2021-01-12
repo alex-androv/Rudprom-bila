1a06a65048f3fe9b4e28fb3bfbb22d83f0f13b3d
alex-androv <alex.androv37@yahoo.com>
2021-01-12 17:10:09 +0300

    beautify files

--- script.js
+++ script.js
@@ -2,29 +2,28 @@ let popup = document.querySelector('.sidebar');
 let openBtn = document.querySelector('.openbtn');
 let closeBtn = popup.querySelector('.closebtn');
 
-openBtn.addEventListener('click', function (evt){
+openBtn.addEventListener('click', function (evt) {
   evt.preventDefault();
-  popup.classList.add('sidebar-opn');  
+  popup.classList.add('sidebar-opn');
   openBtn.classList.add('openbtnon');
-    });
-    
-  closeBtn.addEventListener('click', function(){
-  popup.classList.remove('sidebar-opn');  
+});
+
+closeBtn.addEventListener('click', function () {
+  popup.classList.remove('sidebar-opn');
   openBtn.classList.remove('openbtnon');
-    });
+});
 
-    let upButton = document.querySelector('.up-button');
+let upButton = document.querySelector('.up-button');
 
-    window.onscroll = function () {
-      if (window.pageYOffset > 2000) {
-        upButton.classList.add('shown');
-      } else {
-        upButton.classList.remove('shown');
-      }
-    
-    };
-    
-    upButton.onclick = function () {
-      window.scrollTo(0, 0);
-    };
-    
+window.onscroll = function () {
+  if (window.pageYOffset > 2000) {
+    upButton.classList.add('shown');
+  } else {
+    upButton.classList.remove('shown');
+  }
+
+};
+
+upButton.onclick = function () {
+  window.scrollTo(0, 0);
+};
